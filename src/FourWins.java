98ca534262566493476d6e01c29a151dfb793dc8
Fabian Keßler <malikfk100@gmail.com>
2022-11-23 15:17:31 +0100

    Initial commit

---
+++ src/FourWins.java
@@ -0,0 +1,5 @@
+public class FourWins {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/Game.java
@@ -0,0 +1,9 @@
+public class Game {
+    
+    public int[][] field = new int[7][6];
+
+    public void put(int position) {
+        
+    }
+
+}
