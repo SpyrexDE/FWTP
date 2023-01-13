2178dded2955e73ef2cbf4623b01e553734678a8
SpyrexDE <malikfk100@gmail.com>
2023-01-13 22:14:57 +0100

    Implement win marker

--- src/Game.java
+++ src/Game.java
@@ -31,7 +31,7 @@ public class Game {
         boolean myTurn = con.isHosting();
         boolean turnCompleted = false;
     
-        cli.drawField(field);
+        cli.drawField(field, true);
     
         while (true) {
             if(!myTurn) {
@@ -56,6 +56,19 @@ public class Game {
                 }
             }
             if(turnCompleted) {
+                int won = checkForWin(field);
+                if(won != 0) {
+                    applyWinMarkers(field, won);
+                    cli.drawField(field, true);
+                    if(won == 1) {
+                        cli.success("You won!", "Congratulations!");
+                    } else {
+                        cli.error("You lost!", "Better luck next time!");
+                    }
+                    break;
+                }
+
+                applyWarnings(field);
                 cli.redraw(field);
                 myTurn = !myTurn;
                 turnCompleted = false;
@@ -66,6 +79,122 @@ public class Game {
         }
     }
     
+    // Mark only the four fields in a row and that made the player win the game
+    private void applyWinMarkers(int[][] field, int won) {
+        // Check for horizontal win
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length - 3; j++) {
+                if(field[i][j] == won && field[i][j+1] == won && field[i][j+2] == won && field[i][j+3] == won) {
+                    field[i][j] = won + 4;
+                    field[i][j+1] = won + 4;
+                    field[i][j+2] = won + 4;
+                    field[i][j+3] = won + 4;
+                    return;
+                }
+            }
+        }
+        // Check for vertical win
+        for (int i = 0; i < field.length - 3; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if(field[i][j] == won && field[i+1][j] == won && field[i+2][j] == won && field[i+3][j] == won) {
+                    field[i][j] = won + 4;
+                    field[i+1][j] = won + 4;
+                    field[i+2][j] = won + 4;
+                    field[i+3][j] = won + 4;
+                    return;
+                }
+            }
+        }
+        // Check for diagonal win
+        for (int i = 0; i < field.length - 3; i++) {
+            for (int j = 0; j < field[i].length - 3; j++) {
+                if(field[i][j] == won && field[i+1][j+1] == won && field[i+2][j+2] == won && field[i+3][j+3] == won) {
+                    field[i][j] = won + 4;
+                    field[i+1][j+1] = won + 4;
+                    field[i+2][j+2] = won + 4;
+                    field[i+3][j+3] = won + 4;
+                    return;
+                }
+            }
+        }
+        // Check for diagonal win
+        for (int i = 0; i < field.length - 3; i++) {
+            for (int j = 3; j < field[i].length; j++) {
+                if(field[i][j] == won && field[i+1][j-1] == won && field[i+2][j-2] == won && field[i+3][j-3] == won) {
+                    field[i][j] = won + 4;
+                    field[i+1][j-1] = won + 4;
+                    field[i+2][j-2] = won + 4;
+                    field[i+3][j-3] = won + 4;
+                    return;
+                }
+            }
+        }
+    }
+
+    // If someone would win in the next turn, places a 3 in the field that would make player 1 win and a 4 in the field that would make player 2 win
+    private void applyWarnings(int[][] field) {
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if(field[i][j] == 0) {
+                    
+                    field[i][j] = 1;
+                    if(checkForWin(field) == 1) {
+                        field[i][j] = 3;
+                        return;
+                    }
+
+                    field[i][j] = 2;
+                    if(checkForWin(field) == 2) {
+                        field[i][j] = 4;
+                        return;
+                    }
+                    
+                    // Reset if no one would win
+                    field[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    // Returns 0 if no one won, 1 if player 1 won and 2 if player 2 won
+    private int checkForWin(int[][] field) {
+        // check for horizontal win
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length - 3; j++) {
+                if(field[i][j] != 0 && field[i][j] == field[i][j + 1] && field[i][j] == field[i][j + 2] && field[i][j] == field[i][j + 3]) {
+                    return field[i][j];
+                }
+            }
+        }
+    
+        // check for vertical win
+        for (int i = 0; i < field.length - 3; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if(field[i][j] != 0 && field[i][j] == field[i + 1][j] && field[i][j] == field[i + 2][j] && field[i][j] == field[i + 3][j]) {
+                    return field[i][j];
+                }
+            }
+        }
+    
+        // check for diagonal win
+        for (int i = 0; i < field.length - 3; i++) {
+            for (int j = 0; j < field[i].length - 3; j++) {
+                if(field[i][j] != 0 && field[i][j] == field[i + 1][j + 1] && field[i][j] == field[i + 2][j + 2] && field[i][j] == field[i + 3][j + 3]) {
+                    return field[i][j];
+                }
+            }
+        }
+    
+        // check for diagonal win
+        for (int i = 0; i < field.length - 3; i++) {
+            for (int j = 3; j < field[i].length; j++) {
+                if(field[i][j] != 0 && field[i][j] == field[i + 1][j - 1] && field[i][j] == field[i + 2][j - 2] && field[i][j] == field[i + 3][j - 3]) {
+                    return field[i][j];
+                }
+            }
+        }
+        return 0;
+    }
 
     public boolean put(int position, boolean enemy) {
         if(position < 1 || position > getFieldWidth()) {
@@ -78,7 +207,8 @@ public class Game {
 
         // put at the bottom
         for (int i = getFieldHeight() - 1; i >= 0; i--) {
-            if(field[i][position - 1] == 0) {
+            // if the field is empty (not filled by player 1 or 2)
+            if(field[i][position - 1] != 1 && field[i][position - 1] != 2) {
                 field[i][position - 1] = enemy ? 2 : 1;
                 break;
             }
--- src/cli/Cli.java
+++ src/cli/Cli.java
@@ -11,9 +11,13 @@ public class Cli {
     }
 
     public void redraw(int[][] field) {
+        redraw(field, true);
+    }
+
+    public void redraw(int[][] field, boolean draw_warning) {
         clearConsole();
 
-        drawField(field);
+        drawField(field, draw_warning);
     }
 
     // Get terminal input
@@ -28,7 +32,7 @@ public class Cli {
         System.out.print("\033[H\033[2J");
     }
 
-    public void drawField(int[][] field) {
+    public void drawField(int[][] field, boolean draw_warning) {
         System.out.println("\033[1;36m ╭───┬───┬───┬───┬───┬───┬───╮ \033[0m");
         for (int i = 0; i < 6; i++) {
             System.out.print("\033[1;36m │");
@@ -37,6 +41,14 @@ public class Cli {
                     System.out.print(" \033[0;94mO \033[1;36m│");
                 } else if(field[i][j] == 2) {
                     System.out.print(" \033[0;91mX \033[1;36m│");
+                } else if(field[i][j] == 3 && draw_warning) {
+                    System.out.print(" \033[0;32m⚠ \033[1;36m│");
+                } else if(field[i][j] == 4 && draw_warning) {
+                    System.out.print(" \033[0;33m⚠ \033[1;36m│");
+                } else if(field[i][j] == 5) {
+                    System.out.print(" \033[0;94m\033[9;1;3mO\033[0m \033[1;36m│");
+                } else if(field[i][j] == 6) {
+                    System.out.print(" \033[0;91m\033[9;1;3mX\033[0m \033[1;36m│");
                 } else {
                     System.out.print("   │");
                 }
@@ -56,5 +68,9 @@ public class Cli {
 
     public void error(String message, String value) {
         System.out.println("\033[1;31m" + message + ":\n\033[0;31m" + value + "\033[0m");
+    }
+
+    public void success(String message, String value) {
+        System.out.println("\033[1;32m" + message + ":\n\033[0;32m" + value + "\033[0m");
     }
 }
