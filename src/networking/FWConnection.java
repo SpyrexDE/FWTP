71a18f01b7a0968c5fce40ad74856b732552f4e6
Fabian Keßler <malikfk100@gmail.com>
2023-01-19 13:13:07 +0100

    Add lists of previous hosts list

--- src/networking/FWConnection.java
+++ src/networking/FWConnection.java
@@ -1,8 +1,12 @@
 package networking;
 
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
 
 import cli.Cli;
+import utils.Ansi;
 
 import java.io.BufferedReader;
 
@@ -14,6 +18,14 @@ public class FWConnection {
     public FWConnection() {
         try {
             this.socket = new FWSocket();
+
+            Cli.print(Ansi.Yellow.colorize("Previous hosts were:"));
+
+
+            String[] hosts = getCachedHosts();
+            for (int i = 0; i < hosts.length; i++) {
+                Cli.print(Integer.toString(i) + ". " + Ansi.Cyan.colorize(hosts[i]));
+            }
 
             Cli.print("Enter a valid ip if you want to connect to someone, press enter to host: ");
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,6 +39,8 @@ public class FWConnection {
                 this.socket.connect(s);
                 this.hosting = false;
             }
+
+            addToCachedHosts(s);
 
         } catch (Exception e) {
             Cli.error("Failed to connect to peer", "The connection could not be established.");
@@ -46,6 +60,30 @@ public class FWConnection {
             }
         } catch (Exception e) {
             Cli.error("Failed to connect to peer", "The connection could not be established.");
+        }
+    }
+
+    public String[] getCachedHosts() {
+        Preferences prefs = Preferences.userNodeForPackage(this.getClass());
+
+        final String PREF_NAME = "HOSTS_CACHE";
+        String[] propertyValue = prefs.get(PREF_NAME, "").split("\\|");
+    
+        return propertyValue;
+    }
+
+    public void addToCachedHosts(String host) {
+        Preferences prefs = Preferences.userNodeForPackage(this.getClass());
+        
+        final String PREF_NAME = "HOSTS_CACHE";
+        String newValue = prefs.get(PREF_NAME, "") + host + "|";
+        
+        prefs.put(PREF_NAME, newValue);
+
+        try {
+            prefs.flush();
+        } catch (BackingStoreException e) {
+            e.printStackTrace();
         }
     }
 
